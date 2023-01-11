2d6c464d674f61cb401badaeb3658ffa5611f05c
Matthew-A-Watkins <mattheww@tkins.me>
2023-01-11 15:31:20 -0500

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
+import os
 import sys
 import numpy as np
 import pygame
@@ -7,6 +7,7 @@ import pygame
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+YELLOW = (255, 255, 0)
 
 WIDTH = 1000
 HEIGHT = 1000
@@ -17,25 +18,34 @@ HEIGHT = 1000
 class Pendulum:
 
     # everything is in SI units (kg, Meters, seconds...)
-    # angle in radians
-    def __init__(self, color, window, height=100, width=20, gravity=True, mass=5, angle=0):
+    def __init__(self, color, window, height=100, width=10, gravity=True, mass=5, angle=0, omega=0):
         self.color = color
         self.height = height
         self.width = width
         self.window = window
         self.angle = angle
         self.mass = mass
+        self.omega = omega
         self.gravity = gravity
+
+        self.vx = 0
+        self.vy = 0
         self.x = int(WIDTH / 2) - width/2
         self.y = int(HEIGHT / 2) - height
     def draw(self):
-        print(self.x, self.y)
-        pygame.draw.line(self.window, self.color, (self.x, self.y),
-                         (self.x+self.width*np.sin(self.angle), self.y+self.height*np.cos(self.angle)), self.width)
 
-    def move(self, new_pos: tuple):
-        self.x += new_pos[0]
-        self.y += new_pos[1]
+        end_pos_x = self.x - self.height*np.cos(self.angle*2*np.pi)
+        end_pos_y = self.y - self.height * np.sin(self.angle * 2 * np.pi)
+        pygame.draw.line(self.window, self.color, (self.x, self.y),
+                         (end_pos_x, end_pos_y), self.width)
+        print(end_pos_x, end_pos_y)
+
+    def move(self, new_pos: tuple = (0,0)):
+        self.x += new_pos[0] + self.vx
+        self.y += new_pos[1] + self.vy
+
+    def rotate(self, angle_addition: float =0):
+        self.angle += angle_addition + self.omega
 
 
 def main():
@@ -49,20 +59,38 @@ def main():
     clock = pygame.time.Clock()
 
     pend = Pendulum(WHITE, screen)
-    pends = []
-    pends.append(pend)
+    pends = [pend]
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        try:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-        for obj in pends:
-            obj.draw()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_SPACE:
+                        pygame.quit()
+                        sys.exit()
 
 
-        pygame.display.flip()
-        clock.tick(60)
+            screen.fill(BLACK)
+            #for obj in pends:
+            pend.rotate(0.01)
+            print(pend.angle)
+
+            for obj in pends:
+                obj.draw()
+
+
+            pygame.display.flip()
+            clock.tick(60)
+
+        # I hated having to force quit windows when an error appeared
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
 
 
 if __name__ == '__main__':
