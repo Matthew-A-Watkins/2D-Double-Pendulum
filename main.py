258a330edee151d60be03cf50e1721d9e8518b90
Matthew Watkins <mattheww@tkins.me>
2023-01-11 23:29:13 -0500

    update

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ HEIGHT = 1000
 class Pendulum:
 
     # everything is in SI units (kg, Meters, seconds...)
-    def __init__(self, color, window, height=100, width=10, gravity=True, mass=5, angle=0, omega=0):
+    def __init__(self, color, window, height=100, width=2, gravity=True, mass=5, angle=0, omega=0):
         self.color = color
         self.height = height
         self.width = width
@@ -27,27 +27,74 @@ class Pendulum:
         self.mass = mass
         self.omega = omega
         self.gravity = gravity
-
+        self.cm = (0, 0)
         self.vx = 0
         self.vy = 0
         self.x = int(WIDTH / 2) - width/2
         self.y = int(HEIGHT / 2) - height
+        self.end_pos_x = 0
+        self.end_pos_y = 0
     def draw(self):
 
-        end_pos_x = self.x - self.height*np.cos(self.angle*2*np.pi)
-        end_pos_y = self.y - self.height * np.sin(self.angle * 2 * np.pi)
-        pygame.draw.line(self.window, self.color, (self.x, self.y),
-                         (end_pos_x, end_pos_y), self.width)
-        print(end_pos_x, end_pos_y)
+        self.end_pos_x = self.x + self.height * np.sin(self.angle*2*np.pi)
+        self.end_pos_y = self.y + self.height * np.cos(self.angle * 2 * np.pi)
 
-    def move(self, new_pos: tuple = (0,0)):
+        # i am lazy
+        self.check_bounds(self.end_pos_x, self.end_pos_y, self.x, self.y)
+
+
+        pygame.draw.line(self.window, self.color, (self.x, self.y),
+                         (self.end_pos_x, self.end_pos_y), self.width)
+
+        # CM
+        dx = self.x + self.height * np.sin(self.angle*2*np.pi)/2
+        dy = self.y + self.height * np.cos(self.angle * 2 * np.pi)/2
+        self.cm = (dx, dy)
+        pygame.draw.circle(self.window, RED, (dx, dy), self.width*2)
+
+    def move(self, new_pos: tuple = (0, 0)):
         self.x += new_pos[0] + self.vx
         self.y += new_pos[1] + self.vy
 
-    def rotate(self, angle_addition: float =0):
-        self.angle += angle_addition + self.omega
+    def rotate(self, angle_addition: float = 0):
+        if not self.check_bounds(self.end_pos_x, self.end_pos_y, self.x, self.y):
+            self.angle += angle_addition + self.omega
 
+    def check_bounds(self, ex, ey, x, y):
+        out_of_bounds = 0
 
+        if self.y >= HEIGHT-self.height:
+            self.y = HEIGHT-self.height
+            out_of_bounds += 1
+        elif y <= 0-self.height:
+            self.y = 0-self.height
+            out_of_bounds += 1
+
+        if self.x >= WIDTH-self.width:
+            self.x = WIDTH-self.width
+            out_of_bounds += 1
+
+        elif self.x <= 0-self.height:
+            self.x = 0-self.height
+            out_of_bounds += 1
+
+        if self.end_pos_y >= HEIGHT+self.height:
+            self.end_pos_y = HEIGHT+self.height
+            out_of_bounds += 1
+
+        elif self.end_pos_y <= 0-self.height:
+            self.end_pos_y = 0-self.height
+            out_of_bounds += 1
+
+        if self.end_pos_x >= WIDTH-self.width:
+            self.end_pos_x = WIDTH-self.width
+            out_of_bounds += 1
+
+        elif self.end_pos_x <= 0+self.width:
+            self.end_pos_x = 0+self.width
+            out_of_bounds += 1
+
+        return out_of_bounds >= 2
 def main():
     pygame.init()
 
@@ -79,6 +126,8 @@ def main():
             pend.rotate(0.01)
             print(pend.angle)
 
+            for obj in pends:
+                obj.move((3, 3))
             for obj in pends:
                 obj.draw()
 
