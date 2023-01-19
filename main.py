2d48bc9d07799e211e604d2aa22597ec3f8b065a
Matthew-A-Watkins <mattheww@tkins.me>
2023-01-19 12:09:32 -0500

    update

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ YELLOW = (255, 255, 0)
 WIDTH = 1000
 HEIGHT = 500
 
-class Point:
 
-    def __int__(self, x, y, vx, vy, vw, theta):
+class MyPoint:
+    def __init__(self, x, y, vx, vy, vw, theta):
         self.x = x
         self.y = y
         self.vx = vx
@@ -23,9 +23,8 @@ class Point:
         self.theta = theta
 
 
-
 class Object:
-    def __init__(self, objs:list):
+    def __init__(self, objs: list):
         self.objs = objs
 
     def move(self, vx, vy, vw):
@@ -41,7 +40,14 @@ class Object:
         total_vx += vx
         total_vy += vy
         total_vw += vw
-
+    def draw(self, color, surface):
+        last_point = None
+        for point in self.objs:
+            print(last_point, point)
+            if last_point is not None:
+                print("drawing {} to {}".format(last_point, (point.x, point.y)))
+                pygame.draw.line(surface, color, last_point, (point.x, point.y), 5)
+            last_point = (point.x, point.y)
 
 
 def main():
@@ -55,6 +61,7 @@ def main():
     clock = pygame.time.Clock()
 
     while True:
+        obj = Object([MyPoint(WIDTH/2, HEIGHT/2, 0, 0, 0, 0), MyPoint(10, 10, 0, 0, 0, 0)])
         try:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -67,8 +74,9 @@ def main():
                         pygame.quit()
                         sys.exit()
 
-
             screen.fill(BLACK)
+
+            obj.draw(WHITE, screen)
 
             pygame.display.flip()
 
@@ -79,6 +87,8 @@ def main():
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
+            pygame.quit()
+            sys.exit()
 
 
 if __name__ == '__main__':
